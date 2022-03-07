8cbba9551cbc8376306972aaea1fbc635b24cfaf
konnan933 <konnan933@gmail.com>
2022-03-07 14:49:58 +0100

    3. név deklarárása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -1,9 +1,8 @@
-
 package konfliktuskezeles;
 public class KonfliktusKezeles {
     public static void main(String[] args) {
         String nev1 =  "Attila";
         String nev2 = "X.Y";
-    }
-    
+        String nev3;
+    }  
 }
