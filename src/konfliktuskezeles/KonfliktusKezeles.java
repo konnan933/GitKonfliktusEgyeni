f5553873a1f1370bc31990344fc4c6603c438d9b
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-03-07 14:47:00 +0100

    Nev3 deklarárása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -2,8 +2,10 @@
 package konfliktuskezeles;
 public class KonfliktusKezeles {
     public static void main(String[] args) {
+        int i = 7;
         String nev1 =  "Attila";
         String nev2 = "X.Y";
+        int nev3;
     }
     
 }
