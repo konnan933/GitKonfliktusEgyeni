94bb3d0422e927f7a1bb0b711a2bf0520d80a8cf
konnan933 <konnan933@gmail.com>
2022-03-07 14:45:10 +0100

    Kezelt konfliktus nevek

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -1,12 +1,8 @@
 
 package konfliktuskezeles;
-
-
 public class KonfliktusKezeles {
-
-   
     public static void main(String[] args) {
-        String nev1;
+        String nev1 =  "Attila";
         String nev2 = "X.Y";
     }
     
